03fbf7a1fb54460728ee8bf96d781310ba7b7353
Jesse1439 <asdjkl.key@gmail.com>
2022-04-16 00:35:23 -0300

    añadiendo para despues

--- src/app/routes/about.js
+++ src/app/routes/about.js
@@ -1,10 +1,13 @@
 const badges = require('../../database/badges.json');
+const curses = require('../../database/curses.json');
 
 module.exports = (app) => {
     app.get('/about', (req, res) => {
         res.render("about", {
             path: "",
-            badges: badges
+            badges,
+            curses
+
         })
     })
 }
